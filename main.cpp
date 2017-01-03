f47e2de83c1b8c68869352612dfac30ee5adccf8
Stiju <git@stiju.com>
2017-01-03 21:45:38 +0100

    run all solutions

--- main.cpp
+++ main.cpp
@@ -6,26 +6,26 @@
 int main() {
 	std::ios::sync_with_stdio(false);
 	StopWatch sw;
-	//RUN(1a);
-	//RUN(1b);
-	//RUN(2a);
-	//RUN(2b);
-	//RUN(3a);
-	//RUN(3b);
-	//RUN(4a);
-	//RUN(4b);
-	//RUN(5a);
-	//RUN(5b);
-	//RUN(6a);
-	//RUN(6b);
-	//RUN(7a);
-	//RUN(7b);
-	//RUN(8a);
-	//RUN(9a);
-	//RUN(9b);
-	//RUN(10a);
+	RUN(1a);
+	RUN(1b);
+	RUN(2a);
+	RUN(2b);
+	RUN(3a);
+	RUN(3b);
+	RUN(4a);
+	RUN(4b);
+	RUN(5a);
+	RUN(5b);
+	RUN(6a);
+	RUN(6b);
+	RUN(7a);
+	RUN(7b);
+	RUN(8a);
+	RUN(9a);
+	RUN(9b);
+	RUN(10a);
 	//RUN(11a);
-	//RUN(12a);
+	RUN(12a);
 	RUN(13a);
 }
 
