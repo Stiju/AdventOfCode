9010a682d4b1ad82a05e8eaabe89816b8e5ff3f8
Stiju <git@stiju.com>
2017-01-07 01:06:41 +0100

    day 14 cleanup

--- solutions/day14a.cpp
+++ solutions/day14a.cpp
@@ -5,7 +5,7 @@ namespace day14a {
 	uint32_t byteswap(uint32_t value) {
 		return (value & 0xff) << 24 | (value & 0xff00) << 8 | ((value >> 8) & 0xff00) | ((value >> 24) & 0xff);
 	}
-	bool is_sequence(char needle, const char* haystack, int count) {
+	bool is_sequence(const char* haystack, char needle, int count) {
 		for(int i = 1; i < count; ++i) {
 			if(haystack[i] != needle) {
 				return false;
@@ -15,7 +15,7 @@ namespace day14a {
 	}
 	char find_sequence(const char* haystack, char needle, int count) {
 		for(int i = 0; i < 33 - count; ++i) {
-			if((needle == 0 || haystack[i] == needle) && is_sequence(haystack[i], haystack + i, count)) {
+			if((needle == 0 || haystack[i] == needle) && is_sequence(haystack + i, haystack[i], count)) {
 				return haystack[i];
 			}
 		}
@@ -25,49 +25,34 @@ namespace day14a {
 		int index;
 		char md5[36];
 	};
-	auto hash_and_stretch(const char *key, int stretch) {
-		auto hash = md5(key, strlen(key));
-		hash.chunks.a = byteswap(hash.chunks.a);
-		hash.chunks.b = byteswap(hash.chunks.b);
-		hash.chunks.c = byteswap(hash.chunks.c);
-		hash.chunks.d = byteswap(hash.chunks.d);
-		for(int i = 0; i < stretch; ++i) {
-			char buffer[36];
-			std::snprintf(buffer, 36, "%.8x%.8x%.8x%.8x", hash.chunks.a, hash.chunks.b, hash.chunks.c, hash.chunks.d);
-			hash = md5(buffer, 32);
+	void hash_and_stretch(const char *key, int index, int stretch, MD5Cache& cache) {
+		int length = std::snprintf(cache.md5, 36, "%s%d", key, index);
+		for(int i = 0; i <= stretch; ++i) {
+			auto hash = md5(cache.md5, length);
 			hash.chunks.a = byteswap(hash.chunks.a);
 			hash.chunks.b = byteswap(hash.chunks.b);
 			hash.chunks.c = byteswap(hash.chunks.c);
 			hash.chunks.d = byteswap(hash.chunks.d);
+			length = std::snprintf(cache.md5, 36, "%.8x%.8x%.8x%.8x", hash.chunks.a, hash.chunks.b, hash.chunks.c, hash.chunks.d);
 		}
-		return hash;
+		cache.index = index;
 	}
 
 	uint32_t get_index(const std::string& input, int stretch) {
-		size_t size = input.size();
-		char key[64], *key_end = key + size;
-		std::memcpy(key, input.c_str(), size);
-		size = sizeof(key) - size;
-		std::array<MD5Cache, 1000> hash_cache;
-		std::memset(hash_cache.data(), 0, sizeof(hash_cache));
+		const char* key = input.c_str();
+		std::vector<MD5Cache> hash_cache(1000);
 		hash_cache[0].index = -1;
 		for(int i = 0, found = 0;; ++i) {
 			auto& hc = hash_cache[i % 1000];
 			if(hc.index != i) {
-				hc.index = i;
-				std::snprintf(key_end, size, "%d", i);
-				auto hash = hash_and_stretch(key, stretch);
-				std::snprintf(hc.md5, 36, "%.8x%.8x%.8x%.8x", hash.chunks.a, hash.chunks.b, hash.chunks.c, hash.chunks.d);
+				hash_and_stretch(key, i, stretch, hc);
 			}
 			char c = find_sequence(hc.md5, 0, 3);
 			if(c != 0) {
 				for(int j = i + 1; j <= i + 1000; ++j) {
 					auto& hc = hash_cache[j % 1000];
 					if(hc.index != j) {
-						hc.index = j;
-						std::snprintf(key_end, size, "%d", j);
-						auto hash = hash_and_stretch(key, stretch);
-						std::snprintf(hc.md5, 36, "%.8x%.8x%.8x%.8x", hash.chunks.a, hash.chunks.b, hash.chunks.c, hash.chunks.d);
+						hash_and_stretch(key, j, stretch, hc);
 					}
 					if(find_sequence(hc.md5, c, 5)) {
 						if(++found == 64) {
@@ -76,7 +61,6 @@ namespace day14a {
 						break;
 					}
 				}
-				
 			}
 		}
 		return 0;
